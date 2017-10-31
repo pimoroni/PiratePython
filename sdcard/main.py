9037e16da301348db8dbe88e738627713cf610e9
Phil Howard <phil@gadgetoid.com>
2017-10-31 15:16:25 +0000

    Initial Commit

---
+++ sdcard/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import time
+
+print("""
+
+Hello World
+
+Welcome to embedded Python on your Pi Zero W.
+
+Edit this file to make amazing things happen.
+Your changes will be loaded instantly!
+
+Drop extra libraries into the libraries folder.
+
+Edit the serial_mode file to change USB serial mode:
+    * debug = Show Python output as it happens!
+    * terminal = Log into a terminal with pirate/p1r4t3 
+      and cat /tmp/python.log
+
+And don't forget to have fun!
+
+ - The Pimoroni Pirate Crew
+ 
+""")
